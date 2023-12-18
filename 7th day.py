36d26a6155da30ec72957cad68e424f655f80eb3
SHAIK KHASIM  BASHA <115825218+khasimbasha5757@users.noreply.github.com>
2023-12-18 13:46:46 +0530

    Add files via upload
    strings

---
+++ 7th day.py
@@ -0,0 +1,16 @@
+#strings
+name="khasim basha"
+friend="shaik"
+another_friend="chotu"
+apple='''he said that \n we are idiots
+print("Hello, ",apple)'''
+print(apple)
+nq="Happy"
+print(nq[0])
+print(nq[1])
+print(nq[2])
+print(nq[3])
+print(nq[4])
+print("Lets use a for loop\n")
+for character in apple:
+          print(character)
